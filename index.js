d0f69be7da092267b5cee52eadcc216811ff2c7b
mohamed12345abdullah <112867836+mohamed12345abdullah@users.noreply.github.com>
2024-01-13 14:36:45 +0200

    cors and parser

--- index.js
+++ index.js
@@ -4,12 +4,26 @@ const app=express();
 
 
 const coursesRoute=require("./routes/courses.routes");
+const ClientRouter=require('./routes/clientRouter');
+
+const body_parser=require("body-parser");
+app.use(express.urlencoded({extended:true}));  
+app.use(body_parser.json()); 
+
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // أو تحديد المصدر الصحيح
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+  
 
 
 
 
 app.use("/courses",coursesRoute); 
-
+app.use("/clients",ClientRouter);
 
 
 
