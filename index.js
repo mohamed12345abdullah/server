88cfcd8544d38b8d3f979320f8f2bffc9008c741
mohamed12345abdullah <112867836+mohamed12345abdullah@users.noreply.github.com>
2024-01-13 15:03:29 +0200

    chat cors

--- index.js
+++ index.js
@@ -1,46 +1,32 @@
-const express= require("express");
-const app=express();
+const express = require("express");
+const app = express();
 
+// Handling OPTIONS requests (preflight)
+app.options('*', (req, res) => {
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.status(200).send();
+});
 
-
-const coursesRoute=require("./routes/courses.routes");
-const ClientRouter=require('./routes/clientRouter');
-
-const body_parser=require("body-parser");
-app.use(express.urlencoded({extended:true}));  
-app.use(body_parser.json()); 
-
-
+// CORS middleware
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // أو تحديد المصدر الصحيح
+    res.header('Access-Control-Allow-Origin', '*'); // or specify the correct origin
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
-  });
-  
+});
 
+// Your existing routes
+const coursesRoute = require("./routes/courses.routes");
+const clientRouter = require('./routes/clientRouter');
+app.use("/courses", coursesRoute);
+app.use("/clients", clientRouter);
 
+app.get("/", (req, res) => {
+    res.end(" start server 2 ");
+});
 
+app.listen(5000, () => {
+    console.log("server 2 start and listen to port 5000");
+});
 
-app.use("/courses",coursesRoute); 
-app.use("/clients",ClientRouter);
-
-
-
-
-
-
-
-
-
-app.get("/", (req,res)=>{
-
-    res.end(" start  server 2 ");
-
-})
-
-
-
-app.listen(5000,()=>{
-    console.log(" server 2 start and listen to port 5000");
-})
