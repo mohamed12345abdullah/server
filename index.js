1779e75f59a6dbeea9337de5dcdffd5d1e40e3d7
mohamed12345abdullah <112867836+mohamed12345abdullah@users.noreply.github.com>
2024-01-13 13:44:41 +0200

    server2

--- index.js
+++ index.js
@@ -1,59 +1,14 @@
-
-// require('dotenv').config();
-const port=4000;    //process.env.PORT;
-
-const express=require("express");
+const express= require("express");
 const app=express();
 
+app.get("/", (req,res)=>{
 
-// modules
-const coursesRoute=require("./routes/courses.routes");
-const ClientRouter=require('./routes/clientRouter')
-const bodyParserXML = require('body-parser-xml');
+    res.end(" start  server 2 ");
 
- //midlle wares
-const body_parser=require("body-parser");
-app.use(express.urlencoded({extended:true}));  
-app.use(body_parser.json());  
-var xmlparser = require('express-xml-bodyparser'); 
-app.use(xmlparser());
-const bodyParser = require('body-parser');
-
-// Parse XML request bodies
-bodyParserXML(bodyParser);
-
-// Parse application/json and application/xml
-app.use(bodyParser.json());
-app.use(bodyParser.xml());
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // أو تحديد المصدر الصحيح
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  });
-  
-
-
-
-
-app.use("/courses",coursesRoute); 
-app.use("/clients",ClientRouter);
-
-
-app.post("/",(req,res)=>{ 
-    // console.log(req.body);  
-    res.end(" done ");
-})
-app.get("/",(req,res)=>{
-    res.end("hello world");
 })
 
-// listen server
-app.listen(port,"127.0.0.1",()=>{ 
-    console.log("server run on port"+port);
-}); 
 
 
-
-
+app.listen(5000,()=>{
+    console.log(" server 2 start and listen to port 5000");
+})
