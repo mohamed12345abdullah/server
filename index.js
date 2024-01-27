09bbd96cfbc3dc8f4a67f7ec0a2bb39653896bb8
mohamed12345abdullah <112867836+mohamed12345abdullah@users.noreply.github.com>
2024-01-27 16:08:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json())
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin',"*" );
 
-    // res.header('Access-Control-Allow-Origin', 'no-cors'); // Replace with your client's origin
+    res.header('Access-Control-Allow-Origin', 'no-cors'); // Replace with your client's origin
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
